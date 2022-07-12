bc7270ee69e40552936b37a36b989701d3bf3eb3
unknown <jainprajwal123@gmail.com>
2022-07-12 21:05:38 +0530

    feat: ckeditor5 with elongated features

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import parse from 'html-react-parser';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+
+
+
 import './App.css';
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
+
+
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+const editorElement = document.querySelector('#editor') as HTMLElement;
+
+const editorConfiguration = {
+    toolbar: {
+        items: [
+            'heading',
+            '|',
+            'fontColor',
+            'fontBackgroundColor',
+            'fontFamily',
+            'fontSize',
+            'bold',
+            'italic',
+            'link',
+            'bulletedList',
+            'numberedList',
+            '|',
+            'outdent',
+            'indent',
+            '|',
+            'code',
+            'codeBlock',
+            'imageUpload',
+            'blockQuote',
+            'mediaEmbed',
+            'undo',
+            'redo'
+        ]
+    },
+};
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    const [answer, setAnswer] = useState<string | null>(null);
+    return (
+        <div className="App">
+            <h2>Using CKEditor 5 from online builder in React</h2>
+            <CKEditor
+                editor={Editor}
+                config={editorConfiguration}
+                data="<p>Hello from CKEditor 5!</p>"
+                onReady={editor => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log('Editor is ready to use!', editor);
+                }}
+                onChange={(event, editor) => {
+                    const data = editor.getData();
+                    console.log({ event, editor, data });
+                }}
+                onBlur={(event, editor) => {
+                    console.log('Blur.', editor);
+                }}
+                onFocus={(event, editor) => {
+                    console.log('Focus.', editor);
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
