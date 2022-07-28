da482ec728cc15196070add7b65d8f26e2ef4d1a
unknown <jainprajwal123@gmail.com>
2022-07-28 09:10:20 +0530

    fix: minor api fix

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
 export const BASE_API = `https://stock-overfloww.herokuapp.com`;
 // export const BASE_API = `http://localhost:8081`;
-export const FINANCYY_API = `http://tube-stox.herokuapp.com`;
+export const FINANCYY_API = `https://tube-stox.herokuapp.com`;
